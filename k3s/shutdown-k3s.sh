0ba43a427ca59f515247e86a0e9b783fd7b22473
John Matthew <john@compunique.com>
2023-03-04 16:17:13 -0800

    k3s improvements

---
+++ k3s/shutdown-k3s.sh
@@ -0,0 +1,13 @@
+eval `ssh-agent`
+ssh-add
+
+source ./servers.sh
+
+ssh $NODE3_IP 'k3s-killall.sh'
+ssh $NODE2_IP 'k3s-killall.sh'
+ssh $NODE1_IP 'k3s-killall.sh'
+ssh $MASTER3_IP 'k3s-killall.sh'
+ssh $MASTER2_IP 'k3s-killall.sh'
+ssh $MASTER1_IP 'k3s-killall.sh'
+
+killall ssh-agent
