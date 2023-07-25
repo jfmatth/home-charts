8c204497feed3ff5cc23946ad96dfd192357d925
John Matthew <john@compunique.com>
2023-07-24 18:41:14 -0700

    1master and 3 nodes

--- k3s/k3s-install.sh
+++ k3s/k3s-install.sh
@@ -3,35 +3,45 @@ ssh-add
 
 source ./servers.sh
 
+# echo "Installing Master1 $MASTER1_IP"
+# k3sup install \
+#   --ip $MASTER1_IP \
+#   --user $USER \
+#   --local-path ~/.kube/config \
+#   --cluster \
+#   --context default \
+#   --k3s-extra-args '--disable local-storage' \
+#   --k3s-version $VER
+
 echo "Installing Master1 $MASTER1_IP"
 k3sup install \
   --ip $MASTER1_IP \
   --user $USER \
   --local-path ~/.kube/config \
-  --cluster \
   --context default \
   --k3s-extra-args '--disable local-storage' \
   --k3s-version $VER
 
-echo "Installing Master2 $MASTER2_IP"
-k3sup join \
-  --ip $MASTER2_IP \
-  --user $USER \
-  --server \
-  --server-ip $MASTER1_IP \
-  --server-user $USER \
-  --k3s-extra-args '--disable local-storage' \
-  --k3s-version $VER
 
-echo "Installing Master3 $MASTER3_IP"
-k3sup join \
-  --ip $MASTER3_IP \
-  --user $USER \
-  --server \
-  --server-ip $MASTER1_IP \
-  --server-user $USER \
-  --k3s-extra-args '--disable local-storage' \
-  --k3s-version $VER
+# echo "Installing Master2 $MASTER2_IP"
+# k3sup join \
+#   --ip $MASTER2_IP \
+#   --user $USER \
+#   --server \
+#   --server-ip $MASTER1_IP \
+#   --server-user $USER \
+#   --k3s-extra-args '--disable local-storage' \
+#   --k3s-version $VER
+
+# echo "Installing Master3 $MASTER3_IP"
+# k3sup join \
+#   --ip $MASTER3_IP \
+#   --user $USER \
+#   --server \
+#   --server-ip $MASTER1_IP \
+#   --server-user $USER \
+#   --k3s-extra-args '--disable local-storage' \
+#   --k3s-version $VER
 
 echo "Joining node1 $NODE1_IP"
 k3sup join \
