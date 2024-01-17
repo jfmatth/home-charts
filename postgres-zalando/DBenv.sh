0c1b9a2e5fb9869996e2793790b9d24dd33641ce
John Matthew <john@compunique.com>
2024-01-17 15:13:31 -0800

    new user for all DB's

--- postgres-zalando/DBenv.sh
+++ postgres-zalando/DBenv.sh
@@ -2,5 +2,5 @@ echo PGHOST=192.168.100.140
 echo PGPORT=$(kubectl get svc/database -n postgres -o jsonpath={.spec.ports[0].nodePort} )
 echo PGDATABASE=$1
 echo PGMASTER=$(kubectl get pods -o jsonpath={.items..metadata.name} -l application=spilo,cluster-name=database,spilo-role=master -n postgres)
-echo PGUSER=django
+echo PGUSER=homedb
 echo PGPASSWORD=$(kubectl get secret django.database -o 'jsonpath={.data.password}' -n postgres | base64 -d)
