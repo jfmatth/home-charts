44ac46e0a7465a3a4c37b9e721aa4a86c4048370
John Matthew <john@compunique.com>
2024-01-17 15:14:54 -0800

    dooh, update script

--- postgres-zalando/DBenv.sh
+++ postgres-zalando/DBenv.sh
@@ -3,4 +3,4 @@ echo PGPORT=$(kubectl get svc/database -n postgres -o jsonpath={.spec.ports[0].n
 echo PGDATABASE=$1
 echo PGMASTER=$(kubectl get pods -o jsonpath={.items..metadata.name} -l application=spilo,cluster-name=database,spilo-role=master -n postgres)
 echo PGUSER=homedb
-echo PGPASSWORD=$(kubectl get secret django.database -o 'jsonpath={.data.password}' -n postgres | base64 -d)
+echo PGPASSWORD=$(kubectl get secret homedb.database -o 'jsonpath={.data.password}' -n postgres | base64 -d)
