fae6e53f7368210ae918d56732c83a2cd08d9fea
John Matthew <john@compunique.com>
2024-01-17 14:44:05 -0800

    create script for postgres info

---
+++ postgres-zalando/DBenv.sh
@@ -0,0 +1,6 @@
+echo PGHOST=192.168.100.140
+echo PGPORT=$(kubectl get svc/database -n postgres -o jsonpath={.spec.ports[0].nodePort} )
+echo PGDATABASE=$1
+echo PGMASTER=$(kubectl get pods -o jsonpath={.items..metadata.name} -l application=spilo,cluster-name=database,spilo-role=master -n postgres)
+echo PGUSER=django
+echo PGPASSWORD=$(kubectl get secret django.database -o 'jsonpath={.data.password}' -n postgres | base64 -d)
