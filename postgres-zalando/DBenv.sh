59b14fc2922bd04cf6d7ec3b5e5d17b386fe2e97
John Matthew <john@compunique.com>
2024-03-03 10:20:19 -0800

    improve DBenv for everything

--- postgres-zalando/DBenv.sh
+++ postgres-zalando/DBenv.sh
@@ -1,6 +1,14 @@
-echo PGHOST=192.168.100.140
-echo PGPORT=$(kubectl get svc/database -n postgres -o jsonpath={.spec.ports[0].nodePort} )
-echo PGDATABASE=$1
-echo PGMASTER=$(kubectl get pods -o jsonpath={.items..metadata.name} -l application=spilo,cluster-name=database,spilo-role=master -n postgres)
-echo PGUSER=homedb
-echo PGPASSWORD=$(kubectl get secret homedb.database -o 'jsonpath={.data.password}' -n postgres | base64 -d)
+PGHOST=192.168.100.140
+PGPORT=$(kubectl get svc/database -n postgres -o jsonpath={.spec.ports[0].nodePort} )
+PGDATABASE=$1
+PGMASTER=$(kubectl get pods -o jsonpath={.items..metadata.name} -l application=spilo,cluster-name=database,spilo-role=master -n postgres)
+PGUSER=homedb
+PGPASSWORD=$(kubectl get secret homedb.database -o 'jsonpath={.data.password}' -n postgres | base64 -d)
+
+echo $PGHOST
+echo $PGPORT
+echo $PGDATABASE
+echo $PGMASTER
+echo $PGUSER
+echo $PGPASSWORD
+echo DATABASE_URL=postgres://$PGUSER:$PGPASSWORD@192.168.100.140:30056/$PGDATABASE?ss
