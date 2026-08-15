b9af7cbb013e07b74bdf93eacfb9eda95ffd1652
John Matthew <john@compunique.com>
2026-08-15 11:22:08 -0700

    misc. improvements.

--- Hyper-V/talos/config.ps1
+++ Hyper-V/talos/config.ps1
@@ -4,16 +4,11 @@ $TalosConfig = [ordered]@{
         K8sEndpoint = "10.10.10.201"   # static endpoint
     }
 
-    Nodes = @{
-        ControlPlane = @($ControlPlaneDHCP)
-        Workers      = @("192.168.50.11", "192.168.50.12")
-    }
-
     Paths = @{
         ConfigDir   = ".\cluster-configs"
-        PatchFile   = "controlplane-patch.yaml"   # used as-is
-        Kubeconfig  = "kubeconfig"
-        talosconfig = $TalosConfig.Paths.ConfigDir + "\" + "talosconfig"
+        PatchFile   = "cp-patch.yaml"   # used as-is
+        Kubeconfig  = Join-Path $TalosConfig.Paths.ConfigDir "kubeconfig"
+        talosconfig = Join-Path $TalosConfig.Paths.ConfigDir "talosconfig"
     }
 
     Steps = @{
